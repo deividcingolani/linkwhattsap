6736916f20a67682728bd483152cd2eff55e0dbd
David <cingolanidavid@gmail.com>
2022-01-07 14:13:08 -0300

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a
+        href="whatsapp://send?text=Nickkgg has invited you to Blaze. Join Blaze today and start off with a € 500 bonus https://blaze.com/r/5m5kv"
+        target="_blank" rel="noreferrer"
+      >
+        Refer and Earn
+      </a>
+      Refer and Earn
     </div>
   );
 }
